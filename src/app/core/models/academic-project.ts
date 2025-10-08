4cc9f42bf96f126ef3f761a268f2a52c3b985fcd
YoucTagh <youc.learn.13@gmail.com>
2025-10-08 17:19:07 +0200

    ➕ تحديث المعلومات وإضافة مشروع بحثي

--- src/app/core/models/academic-project.ts
+++ src/app/core/models/academic-project.ts
@@ -1,6 +1,8 @@
 export interface AcademicProject {
     name: string;
     imgPath: string ;
+    startDate: string;
+    endDate: string;
     description: string;
     sourceUrl: string;
     previewUrl: string;
